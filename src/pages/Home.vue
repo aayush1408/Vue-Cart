3e5b3faf4f4a9d6374660024db5d1667d3f8f7f5
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-17 22:55:10 +0530

    model added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+<div>
+<div class="container">
+  <div class="row">
+        <div v-for="card in cards" :key="card.name" class="col-md-3">
+          <img :src=card.url />
+          <h4>{{card.name}}</h4>          
+          <h5>{{card.price}}</h5>
+          <h5>{{card.type}}</h5>
+          <button class="btn btn-sm btn-success">Add to cart</button>
+        </div>
+    </div>
+</div>
+</div>
 </template>
 
 <script>
@@ -9,7 +19,32 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      cards : [
+        {
+        name:'Dark Magician',
+        url:'https://vignette.wikia.nocookie.net/myyugiohdeck/images/d/da/Dark_Magician.jpg/revision/latest/scale-to-width-down/325?cb=20110610035254',
+        price:'2000',
+        type:'Monster'
+      },
+      {
+        name:'Red Eyes Black Dragon ',
+        url:'https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/22341_200w.jpg',
+        price:'2000',
+        type:'Monster'
+      },
+        {
+        name:'Blue Eye White Dragon ',
+        url:'https://vignette.wikia.nocookie.net/yugioh/images/7/77/BlueEyesWhiteDragon-LCKC-EN-UR-1E.png/revision/latest?cb=20180415030139',
+        price:'4000',
+        type:'Monster'
+      },
+        {
+        name:'Sword Slasher',
+        url:'http://qtoptens.com/wp-content/uploads/2017/06/SwordSlasher.png',
+        price:'1500',
+        type:'Monster'
+      }
+      ]
     }
   }
 }
@@ -17,5 +52,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+img{
+  height: 300px;
+  width: 200px;
+}
 </style>
